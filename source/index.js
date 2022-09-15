db9d51a17cdf9495c83e3d8971326f4757c823ac
surya5602 <suryakgm1234@gmail.com>
2022-09-15 12:29:48 +0530

    h1

--- source/index.js
+++ source/index.js
@@ -1,14 +1,9 @@
-const fs = require('fs');
 const http = require('http');
 const server =  http.createServer((req, res)=>{
        res.end("Hello from the server!");
 });
-server.listen(8080, '0.0.0.0',()=>{
-    console.log("listening to request on port 8080");
-    
-
-})
-
+server.listen(8080);
+   
 
 
 
