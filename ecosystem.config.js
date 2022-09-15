c9d345f88dc77c760b40a1ba4918c6f73829bba4
surya5602 <suryakgm1234@gmail.com>
2022-09-15 10:16:31 +0530

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'surya',
+      host : '192.168.1.62',
+      ref  : 'origin/main',
+      repo : 'https://github.com/surya5602/Task.git',
+      path : '/var/www/deployment',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
